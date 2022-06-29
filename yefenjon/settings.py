19fe1201ccd1aa78adcabbe2273fc90823dde00e
mahdi <mahdi@mahdi>
2022-06-29 16:20:00 +0430

    initial commit

--- yefenjon/settings.py
+++ yefenjon/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_REDIRECT_URL = "account:home"
+LOGIN_URL = "account:login"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'account.apps.AccountConfig',
 	'donate.apps.donateConfig',
     'django.contrib.admin',
     'django.contrib.auth',
