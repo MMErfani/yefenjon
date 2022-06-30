4a30643214579da278a5dd22f305cd1190d048f4
mahdi <mahdi@mahdi>
2022-06-30 17:33:13 +0430

    user model upgraded

--- account/models.py
+++ account/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import os
 
 # Create your models here.
-
-
-
+class User(AbstractUser):
+	prof_id = models.SlugField(max_length=200,verbose_name="آیدی",default="NULL")
+	card_number = models.IntegerField(verbose_name="شماره حساب", default=0)
+	avatar = models.ImageField(upload_to="avatars",verbose_name="آواتار", default='NULL')
+	
 
 	
--- yefenjon/settings.py
+++ yefenjon/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR / 'media'
-#AUTH_USER_MODEL = 'account.User'
+AUTH_USER_MODEL = 'account.User'
 
 #=========EMAIL==========
 # in cmd/file:
