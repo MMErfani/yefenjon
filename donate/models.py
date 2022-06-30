9cf301c68ea1ef28a790cda0100300763a04f532
mahdi <mahdi@mahdi>
2022-06-30 20:50:11 +0430

    we can see  avatars from account panel

--- donate/models.py
+++ donate/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
--- donate/views.py
+++ donate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
+from account.models import User
 
 # Create your views here.
 def index(request):
