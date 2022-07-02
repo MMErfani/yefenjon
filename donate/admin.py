9d5188ef153b295647cea33435283f0287c614f8
mahdi <mahdi@mahdi>
2022-07-02 17:00:18 +0430

    peyment system admin panel updated

--- donate/admin.py
+++ donate/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import donate
 # Register your models here.
 
-admin.site.register(donate)
+@admin.register(donate)
+class donateAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'donate_to','name',"payment_status")
+    list_filter = ('payment_status',)
+    search_fields = ('name', 'donate_to')
+    
