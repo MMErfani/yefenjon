a187f9cbc692b07557c80c6dc7fa96de796a054a
mahdi <mahdi@mahdi>
2022-07-02 12:36:49 +0430

    bug fixed

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import User
 from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -14,6 +15,7 @@ class EditProfile(UpdateView):
 	model = User
 	fields = ['first_name', 'last_name', 'username', 'email', 'card_number','avatar','job']
 	template_name = 'registration/edit-profile.html'
+	success_url = reverse_lazy('account:EditProfile')
 	
 	def get_object(self):
 		return User.objects.get(pk = self.request.user.pk)
